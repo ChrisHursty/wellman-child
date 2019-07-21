14ec50031040c4583af1f5d182e8d1ac89dc7fa6
Chris Hursty <chris@iamchrishurst.com>
2019-07-21 16:51:03 -0400

    Facilities responsiveness and template featured images

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -9,23 +9,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
 	<header class="entry-header">
-
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
 		<div class="entry-meta">
-
 			<?php understrap_posted_on(); ?>
-
 		</div><!-- .entry-meta -->
-
 	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
 	<div class="entry-content">
 
 		<?php the_content(); ?>
@@ -38,13 +28,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 			)
 		);
 		?>
-
 	</div><!-- .entry-content -->
-
 	<footer class="entry-footer">
-
 		<?php understrap_entry_footer(); ?>
-
 	</footer><!-- .entry-footer -->
-
 </article><!-- #post-## -->
--- page.php
+++ page.php
@@ -13,51 +13,43 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
 get_header();
-
 $container     = get_theme_mod( 'understrap_container_type' );
 $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'page-header' ); 
 ?>
 
+<?php if ( has_post_thumbnail() ) { ?>
 <div class="container-fluid header-image" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat; background-size: cover; background-position: 50% 50%;">
 	<div class="page-title">
 		<h1><?php the_title(); ?></h1>
 	</div>
 </div>
+<?php } else { ?>
+<div class="container-fluid header-image" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/images/default-hero.jpg; ?>') no-repeat; background-size: cover; background-position: 50% 50%;">
+	<div class="page-title">
+		<h1><?php the_title(); ?></h1>
+	</div>
+</div>
+<?php } ?>
 <div class="wrapper" id="page-wrapper">
-
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
 		<div class="row">
-
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
 			<main class="site-main" id="main">
-
 				<?php while ( have_posts() ) : the_post(); ?>
-
 					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
 					<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
 					endif;
 					?>
-
 				<?php endwhile; // end of the loop. ?>
-
 			</main><!-- #main -->
-
 			<!-- Do the right sidebar check -->
 			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
 		</div><!-- .row -->
-
 	</div><!-- #content -->
-
 </div><!-- #page-wrapper -->
-
 <?php get_footer(); ?>
