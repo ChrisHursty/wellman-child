0ef4209c052954536aa6417c361c4f37715ac619
Chris Hursty <chris@iamchrishurst.com>
2019-07-26 11:20:33 -0400

    Updated facilities archive and scss

--- archive-facilities.php
+++ archive-facilities.php
@@ -26,14 +26,16 @@ get_header();
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 				<div class="col-12 col-md-4">
-					<a href="<?php echo the_permalink(); ?>">
-						<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-							<div class="page-title">
-								<h2><?php the_title(); ?></h2>
-							</div>
-							<?php echo get_the_post_thumbnail( $post->ID, 'archive-thumbnail' ); ?>
-						</article><!-- #post-## -->
-					</a>
+					<div class="card-container">
+						<a href="<?php echo the_permalink(); ?>">
+							<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+								<div class="page-title">
+									<h2><?php the_title(); ?></h2>
+								</div>
+								<?php echo get_the_post_thumbnail( $post->ID, 'archive-thumbnail' ); ?>
+							</article><!-- #post-## -->
+						</a>
+					</div>
 				</div>
 				<?php endwhile; ?>
 			<?php else : ?>
