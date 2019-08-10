a6e7933d447690e80c9211d1b1e6effa5a209126
Chris Hursty <chris@iamchrishurst.com>
2019-08-10 15:29:03 -0400

    Added more archive pages

--- archive-events.php
+++ archive-events.php
@@ -14,27 +14,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 
 ?>
-<div class="container-fluid header-image" style="background: url('<?php bloginfo('stylesheet_directory'); ?>/images/facilities-hero.jpg') no-repeat; background-size: cover; background-position: 50% 50%;">
+<div class="container-fluid header-image" style="background: url('<?php bloginfo('stylesheet_directory'); ?>/images/events-hero.jpg') no-repeat; background-size: cover; background-position: 50% 50%;">
 	<div class="page-title">
 		<h1>Events</h1>
 	</div>
 </div>
-<div class="wrapper" id="archive-wrapper">
-	<div class="container-fluid" id="content" tabindex="-1">
+<div class="events-archive wrapper" id="archive-wrapper">
+	<div class="container" id="content" tabindex="-1">
 		<div class="row">
 			<?php if ( have_posts() ) : ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
-				<div class="col-12 col-md-4">
+				<div class="col-12 col-md-6">
 					<div class="card-container">
-						<a href="<?php echo the_permalink(); ?>">
-							<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+						<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+							
+							<div class="event-details">
 								<div class="page-title">
-									<h2><?php the_title(); ?></h2>
+									<a href="<?php echo the_permalink(); ?>">
+										<h2><?php the_title(); ?></h2>
+									</a>
 								</div>
+								<h5 class="date">Date &amp; Time<br>
+									<span><?php the_field('event_date'); ?></span>
+								</h5>
+								<a href="<?php echo the_permalink(); ?>">Read More >></a>
+							</div>
+							<div class="event-img">
 								<?php echo get_the_post_thumbnail( $post->ID, 'archive-thumbnail' ); ?>
-							</article><!-- #post-## -->
-						</a>
+							</div>
+						</article><!-- #post-## -->
 					</div>
 				</div>
 				<?php endwhile; ?>
